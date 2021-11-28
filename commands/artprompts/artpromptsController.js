e643ba1b41c87d4863010b132f066b4abfc4db3e
totalatomic <63284260+totalatomic@users.noreply.github.com>
2021-11-28 21:43:40 +0100

    Revert "added location prompts"

--- commands/artprompts/artpromptsController.js
+++ commands/artprompts/artpromptsController.js
@@ -1,14 +1,4 @@
 const axios = require("axios");
-const cheerio = require('cheerio');
-
-exports.promptFetcher = async function (params) {
-    //get the full html
-    const { data } = await axios.get(`https://www.artideasgenerator.com${params}`);
-    const $ = cheerio.load(data);
-    //find all of the en class's text
-    const item = $(".en").text()
-    //remove other html tags and split on the new line
-    const arrAttb = item.replace(/<\/?[^>]+(>|$)/g, "").split("\n")
-    //return an array with atributes to be handled in the command itself
-    return arrAttb;
+exports.promptApearence = async function (params) {
+    
 }
