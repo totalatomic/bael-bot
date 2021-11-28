cfe68e2f360cfdd780c2645ef8ab62f9859e4607
totalatomic <63284260+totalatomic@users.noreply.github.com>
2021-11-28 21:26:22 +0100

    added location prompts
    added >locationprompts
    changed the way the prompt commands get handled to make it easier in the future

--- commands/artprompts/artpromptsController.js
+++ commands/artprompts/artpromptsController.js
@@ -1,4 +1,14 @@
 const axios = require("axios");
-exports.promptApearence = async function (params) {
-    
+const cheerio = require('cheerio');
+
+exports.promptFetcher = async function (params) {
+    //get the full html
+    const { data } = await axios.get(`https://www.artideasgenerator.com${params}`);
+    const $ = cheerio.load(data);
+    //find all of the en class's text
+    const item = $(".en").text()
+    //remove other html tags and split on the new line
+    const arrAttb = item.replace(/<\/?[^>]+(>|$)/g, "").split("\n")
+    //return an array with atributes to be handled in the command itself
+    return arrAttb;
 }
